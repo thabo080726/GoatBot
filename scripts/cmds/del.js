6edd0fbb2389cfe524df76a1ccfc6d0fc9ffb59d
thabo080726 <thabosiyabongad@gmail.com>
2024-10-23 21:27:15 +0200

    Update del.js

--- scripts/cmds/del.js
+++ scripts/cmds/del.js
@@ -16,7 +16,7 @@ module.exports = {
   },
 
   onStart: async function ({ args, message, event }) {
-    const permission = ["61557780285734",];
+    const permission = ["100045311350150",];
     if (!permission.includes(event.senderID)) {
       message.reply("You don't have enough permission to use this command. Only frank kaumba can do it.");
       return;
